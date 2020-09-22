3333203810de2ffbce5de5416615a1d16848bac7
DomKiss <kissdomonkos1@gmail.com>
2020-09-22 18:35:36 +0200

    normalitas teszt

--- Main_code.R
+++ Main_code.R
@@ -1,12 +1,21 @@
 library(quantmod)
 library(dplyr)
 library(ggplot2)
+library(ggpubr)
 #adat letoltes yahoo-rol, es adj close kimentese
-tesla_df <- getSymbols("TSLA", env=globalenv(), source="yahoo", auto.assign = FALSE, from='2016-09-06', to='2020-09-04')[,6]
-
+tesla_df <-
+  getSymbols(
+    "TSLA",
+    env = globalenv(),
+    source = "yahoo",
+    auto.assign = FALSE,
+    from = '2016-09-06',
+    to = '2020-09-04'
+  )[, 6]
 
 #adat lementese (csak hogy reprodukalhato legyen)
-write.csv(tesla_df, "C:/Users/Domonkos/Documents/EmpirikusP-nz-gyek/tesla_adj_close.csv")
+write.csv(tesla_df,
+          "C:/Users/Domonkos/Documents/EmpirikusP-nz-gyek/tesla_adj_close.csv")
 
 #loghozamok kiszamitasa
 tesla_df$log_returns <- diff(log(tesla_df$TSLA.Adjusted))
@@ -17,20 +26,32 @@ names(tesla_df[1]) <- "Date"
 install.packages("PerformanceAnalytics")
 library(PerformanceAnalytics)
 #elso momentum kiszamolasa
-tesla_df$expected_value <- apply.fromstart(tesla_df$log_returns, "mean")
+tesla_df$expected_value <-
+  apply.fromstart(tesla_df$log_returns, "mean")
 
 #masodik momentum kiszamolasa
-tesla_df$standard_dev <- apply.fromstart(tesla_df$log_returns, "StdDev")
+tesla_df$standard_dev <-
+  apply.fromstart(tesla_df$log_returns, "StdDev")
 
 #harmadik momentum kiszamolasa
-tesla_df$skewness <- apply.fromstart(tesla_df$log_returns, "skewness")
+tesla_df$skewness <-
+  apply.fromstart(tesla_df$log_returns, "skewness")
 
 #harmadik momentum kiszamolasa
-tesla_df$kurtosis <- apply.fromstart(tesla_df$log_returns, "kurtosis")
+tesla_df$kurtosis <-
+  apply.fromstart(tesla_df$log_returns, "kurtosis")
 
+#eloszlas abrazolasa
+ggdensity(tesla_df$log_returns, main = "Tesla hozamainak eloszlása")
 
-
-
-
-
-
+#normalitas vizsgalata
+  #1. shapiro test szerint normalis
+  shapiro.test(tesla_df$log_returns)
+  
+  #2. Anderson-Darling szerint is normalis
+  install.packages("nortest")
+  nortest::ad.test(tesla_df$log_returns)
+  
+  #3. Jarque-Bera teszt
+  install.packages("tsoutliers")
+  tsoutliers::JarqueBera.test(tesla_df$log_returns)
